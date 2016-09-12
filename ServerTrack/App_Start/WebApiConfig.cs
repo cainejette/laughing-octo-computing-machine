09978a43e7d8207a6c0fdf4b37c31fdec8327aaf
cainejette <caine.jette@ctl.io>
2016-09-12 13:21:24 -0700

    Initial commit.

---
+++ ServerTrack/App_Start/WebApiConfig.cs
@@ -0,0 +1,22 @@
+﻿/*  
+ Licensed under the MIT License. See LICENSE file in the project root for full license information.  
+*/
+using System.Net.Http.Headers;
+using System.Web.Http;
+
+namespace ServerTrack
+{
+    /*
+        Assuming the API will evolve over time
+    */
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
+        }
+    }
+}
---
+++ ServerTrack/Controllers/TrackController.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ServerTrack.Controllers
+{
+    public class TrackController : ApiController
+    {
+        /// <summary>
+        /// Simple result to handle the default browser starting url
+        /// </summary>
+        [Route("")]
+        [HttpGet]
+        public HttpResponseMessage Root()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "ServerTrack is active");
+        }
+    }
+}
